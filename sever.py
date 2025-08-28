fd0daf333f477e0ede69dfecf6d054fc74f6c262
minhduc862005 <duchm9479@ut.edu.vn>
2025-08-28 20:54:31 +0700

    Cấu hình cơ bản

--- sever.py
+++ sever.py
@@ -4,3 +4,12 @@ import os, sqlite3, uuid
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "chat.db")
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "static", "uploads")
+ALLOWED_EXT = {"png","jpg","jpeg","gif","webp"}
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET","change_this_secret_for_prod")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+socketio = SocketIO(app, cors_allowed_origins="*")
