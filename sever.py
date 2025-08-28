7228c415ec3532bf0d8c07cb8ebeff58c2591842
minhduc862005 <duchm9479@ut.edu.vn>
2025-08-28 20:53:41 +0700

    Import thư viện

--- sever.py
+++ sever.py
@@ -1,1 +1,6 @@
-
+from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory, jsonify, flash
+from flask_socketio import SocketIO, emit, join_room, leave_room
+import os, sqlite3, uuid
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
