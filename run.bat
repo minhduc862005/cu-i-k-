6f2b26dcc280d135308a3e086b4794d21d4d99fd
longnh0493-hl <longnh0493@ut.edu.vn>
2025-08-28 21:40:32 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,7 @@
-
+@echo off
+echo ================================
+echo   Starting CuteChatPro server...
+echo   Open: http://127.0.0.1:5000 or http://<your-pc-ip>:5000 for LAN
+echo ================================
+python server.py
+pause
